459cbe436673de113b91f402dd64cddf760ea958
Dustin Horvath <dustinmhorvath@gmail.com>
2016-02-10 17:20:20 -0600

    Initial commit of pipe/fork lab.

---
+++ lab3/ipc/finder.c
@@ -0,0 +1,89 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <strings.h>
+#include <errno.h>
+#include <sys/wait.h>
+
+#define BSIZE 256
+
+#define BASH_EXEC  "/bin/bash"
+#define FIND_EXEC  "/bin/find"
+#define XARGS_EXEC "/usr/bin/xargs"
+#define GREP_EXEC  "/bin/grep"
+#define SORT_EXEC  "/bin/sort"
+#define HEAD_EXEC  "/usr/bin/head"
+
+int main(int argc, char *argv[])
+{
+  int status;
+  pid_t pid_1, pid_2, pid_3, pid_4;
+  int fd1[2];
+  int fd2[2];
+  int fd3[2];
+  int pipe(fd1);
+  int pipe(fd2);
+  int pipe(fd3);
+  char cmdbuf[BSIZE];
+
+  if (argc != 4) {
+    printf("usage: finder DIR STR NUM_FILES\n");
+    exit(0);
+  }
+
+  pid_1 = fork();
+  if (pid_1 == 0) {
+    bzero(cmdbuf, BSIZE);
+    sprintf(cmdbuf, "%s %s -name \'*\'.[ch]", FIND_EXEC, argv[1]);
+    dup2(fd1[1], STDOUT_FILENO);
+    if(execl(FIND_EXEC, cmdbuf, (char *) 0) < 0) {
+      fprintf(stderr, "\nError execing find. ERROR#%d\n", errno);
+      return EXIT_FAILURE;
+    }
+       /* First Child */
+    exit(0);
+  }
+
+  pid_2 = fork();
+  if (pid_2 == 0) {
+    dup2(STDIN_FILENO, fd1[0]);
+
+    if(execl(XARGS_EXEC, GREP_EXEC, cmdbuf, (char *) 0) < 0) {
+      fprintf(stderr, "\nError execing find. ERROR#%d\n", errno);
+      return EXIT_FAILURE;
+    }
+    
+    /* Second Child */
+    exit(0);
+  }
+
+  pid_3 = fork();
+  if (pid_3 == 0) {
+    /* Third Child */
+  }
+
+  pid_4 = fork();
+  if (pid_4 == 0) {
+    /* Fourth Child */
+    exit(0);
+  }
+
+  if ((waitpid(pid_1, &status, 0)) == -1) {
+    fprintf(stderr, "Process 1 encountered an error. ERROR%d", errno);
+    return EXIT_FAILURE;
+  }
+  if ((waitpid(pid_2, &status, 0)) == -1) {
+    fprintf(stderr, "Process 2 encountered an error. ERROR%d", errno);
+    return EXIT_FAILURE;
+  }
+  if ((waitpid(pid_3, &status, 0)) == -1) {
+    fprintf(stderr, "Process 3 encountered an error. ERROR%d", errno);
+    return EXIT_FAILURE;
+  }
+  if ((waitpid(pid_4, &status, 0)) == -1) {
+    fprintf(stderr, "Process 4 encountered an error. ERROR%d", errno);
+    return EXIT_FAILURE;
+  }
+
+  return 0;
+}
