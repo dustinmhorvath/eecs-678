65b24968543f9cbc1b4e2245ccd9fa1276b4ec4d
Dustin Horvath <dustinmhorvath@gmail.com>
2016-04-20 16:29:56 -0500

    Gave it slightly more useful output.

--- lab09/mmio/memmap.c
+++ lab09/mmio/memmap.c
@@ -64,6 +64,7 @@ int main (int argc, char *argv[])
     err_sys("can't get size of file");
   }
   else{
+    printf("Got file size.\n");
     size = statbuf.st_size;
   }
 
@@ -74,6 +75,9 @@ int main (int argc, char *argv[])
   failure = lseek(fdout, size - 1, SEEK_SET);
   if(failure < 0){
     err_sys("Can't read to end of file.");
+  }
+  else{
+    printf("Seeked to end of file space.\n");
   }
 
   /* 
@@ -94,6 +98,8 @@ int main (int argc, char *argv[])
    */
 
   src = mmap(NULL, size, PROT_READ, MAP_SHARED, fdin, 0);
+  printf("Mapped input file.\n");
+
 
   /* 
    * 5. mmap the output file 
@@ -101,6 +107,7 @@ int main (int argc, char *argv[])
 
   //FILE* destination = mmap(NULL, size, PROT_WRITE, MAP_SHARED, fdout, 0);
   dst = mmap(NULL, size, (PROT_READ | PROT_WRITE), MAP_SHARED, fdout, 0);  
+  printf("Mapped output file.\n");
   /* 
    * 6. copy the input file to the output file 
    */
@@ -111,6 +118,8 @@ int main (int argc, char *argv[])
    // *dst = *src;
 
   memcpy(dst, src, size);
+  printf("Copy complete.\n");
+
 
 } 
 
